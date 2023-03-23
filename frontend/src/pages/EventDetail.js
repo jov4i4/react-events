a2b478c92bbd90b8c2f1b79a9f843e37c5089322
jov4i4 <jovan_oh@hotmail.com>
2023-03-23 19:14:38 +0100

    The useRouteLoaderData() hook & accessing data

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,9 +1,9 @@
-import { json, useLoaderData, useParams } from 'react-router-dom';
+import { json, useRouteLoaderData, useParams } from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData('event-detail');
 
   return <EventItem event={data.event} />;
 }
